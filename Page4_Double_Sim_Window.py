639db7f3c1fe89550db433e4ab71ca1ea1e335eb
wuheideyun <32124233+wuheideyun@users.noreply.github.com>
2024-08-29 09:19:55 +0800

    增加第4页内容

--- Page4_Double_Sim_Window.py
+++ Page4_Double_Sim_Window.py
@@ -4,22 +4,19 @@ import os
 from PySide6.QtCore import QSettings, Qt
 from PySide6.QtCore import QCoreApplication
 from PySide6.QtGui import QMovie, QIcon
-from PySide6.QtWidgets import QHBoxLayout, QFrame, QWidget, QGridLayout, QVBoxLayout,  QMainWindow
+from PySide6.QtWidgets import QHBoxLayout, QFrame, QWidget, QGridLayout, QVBoxLayout, QMainWindow
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from qfluentwidgets import PrimaryPushButton, LineEdit, BodyLabel, ImageLabel
 
-from Page1_Sync_Algorithms_Polishing_Distribution_Simulation import Polishing_distribution_Thread
-from Page1_Sync_Algorithms_Generate_Animation import Animation_produce
-from Page1_Sync_Algorithms_Middle_Line_Plot import middle_line_plot
-from Page1_Sync_Algorithms_Equal_Parameter_Calculate import equal_num_calculate
+from Page4_Double_Generate_Animation import Animation_produce_equal, Animation_produce_order, Animation_produce_cross
 
 
 class Page4_Double_Sim_Window(QFrame):
 
     def __init__(self, text: str, parent=None):
         super().__init__(parent=parent)
-        self.customWidth = 100 #自定义宽度
+        self.customWidth = 100  # 自定义宽度
         self.reCalcFlag = True
         self.settings = QSettings("config.ini", QSettings.IniFormat)  # 使用配置文件
 
@@ -33,8 +30,6 @@ class Page4_Double_Sim_Window(QFrame):
         self.gridLayout.setObjectName(u"gridLayout")
 
         # 初始化所有控件（与原代码保持一致）
-
-
 
         self.iconHead = ImageLabel("./images/double2.png", self.centralwidget)
         self.gridLayout.addWidget(self.iconHead, 0, 0, 1, 13)
@@ -131,59 +126,72 @@ class Page4_Double_Sim_Window(QFrame):
         self.lineedit_delay_time.setObjectName(u"lineedit_delay_time")
         self.gridLayout.addWidget(self.lineedit_delay_time, 4, 7, 1, 1)
 
+        # 保存参数
+        self.button_save = PrimaryPushButton(self.centralwidget)
+        self.button_save.setObjectName(u"button_save")
+        self.gridLayout.addWidget(self.button_save, 6, 1, 1, 1)
+
         # 同步摆动画
         self.button_sync_swing_animation = PrimaryPushButton(self.centralwidget)
         self.button_sync_swing_animation.setObjectName(u"button_sync_swing_animation")
         self.gridLayout.addWidget(self.button_sync_swing_animation, 6, 2, 1, 1)
+        self.button_sync_swing_animation.clicked.connect(
+            self.start_computation_trajectory_animation_equal)  # 动画按钮(同步摆模式)
 
         # 同步摆抛磨量分布仿真
         self.button_sync_swing_simulation = PrimaryPushButton(self.centralwidget)
         self.button_sync_swing_simulation.setObjectName(u"button_sync_swing_simulation")
         self.gridLayout.addWidget(self.button_sync_swing_simulation, 6, 3, 1, 3)
+        # self.Button_simulation_equal.clicked.connect(self.start_computation_Polishing_distribution_equal)  # 抛磨量分布仿真按钮
 
         # 同步摆轨迹中心线绘制
         self.button_sync_swing_middle_line = PrimaryPushButton(self.centralwidget)
         self.button_sync_swing_middle_line.setObjectName(u"button_sync_swing_middle_line")
         self.gridLayout.addWidget(self.button_sync_swing_middle_line, 6, 6, 1, 1)
+        # self.Button_middle_line_equal.clicked.connect(self.middle_line_figure_plot_equal)  # 磨头中心线绘制按钮
 
         # 顺序摆动画
         self.button_order_swing_animation = PrimaryPushButton(self.centralwidget)
         self.button_order_swing_animation.setObjectName(u"button_order_swing_animation")
         self.gridLayout.addWidget(self.button_order_swing_animation, 7, 2, 1, 1)
+        self.button_order_swing_animation.clicked.connect(
+            self.start_computation_trajectory_animation_order)  # 动画按钮(顺序摆模式)
 
         # 顺序摆抛磨量分布仿真
         self.button_order_swing_simulation = PrimaryPushButton(self.centralwidget)
         self.button_order_swing_simulation.setObjectName(u"button_order_swing_simulation")
         self.gridLayout.addWidget(self.button_order_swing_simulation, 7, 3, 1, 3)
+        # self.Button_simulation_order.clicked.connect(self.start_computation_Polishing_distribution_order)  # 抛磨量分布仿真按钮
 
         # 顺序摆轨迹中心线绘制
         self.button_order_swing_middle_line = PrimaryPushButton(self.centralwidget)
         self.button_order_swing_middle_line.setObjectName(u"button_order_swing_middle_line")
         self.gridLayout.addWidget(self.button_order_swing_middle_line, 7, 6, 1, 1)
+        # self.Button_middle_line_order.clicked.connect(self.middle_line_figure_plot_order)  # 磨头中心线绘制按钮
 
         # 交叉摆动画
         self.button_cross_swing_animation = PrimaryPushButton(self.centralwidget)
         self.button_cross_swing_animation.setObjectName(u"button_cross_swing_animation")
         self.gridLayout.addWidget(self.button_cross_swing_animation, 8, 2, 1, 1)
+        self.button_cross_swing_animation.clicked.connect(self.start_computation_trajectory_animation_cross)  # 动画按钮(交叉摆模式)
 
         # 交叉抛磨量分布仿真
         self.button_cross_swing_simulation = PrimaryPushButton(self.centralwidget)
         self.button_cross_swing_simulation.setObjectName(u"button_cross_swing_simulation")
         self.gridLayout.addWidget(self.button_cross_swing_simulation, 8, 3, 1, 3)
+        # self.Button_simulation_cross.clicked.connect(self.start_computation_Polishing_distribution_cross)  # 抛磨量分布仿真按钮
 
         # 同步摆轨迹中心线绘制
         self.button_cross_swing_middle_line = PrimaryPushButton(self.centralwidget)
         self.button_cross_swing_middle_line.setObjectName(u"button_cross_swing_middle_line")
         self.gridLayout.addWidget(self.button_cross_swing_middle_line, 8, 6, 1, 1)
+        # self.Button_middle_line_cross.clicked.connect(self.middle_line_figure_plot_cross)  # 磨头中心线绘制按钮
 
         # 动画
         self.widget = QWidget(self.centralwidget)
         self.widget.setObjectName(u"widget")
         self.widget.setStyleSheet(u"background-color: rgb(0, 170, 255);")
         self.gridLayout.addWidget(self.widget, 9, 0, 1, 8)
-
-
-
 
         self.iconBottom = ImageLabel("./images/bottom2.png", self.centralwidget)
         self.gridLayout.addWidget(self.iconBottom, 16, 0, 1, 13)
@@ -218,6 +226,7 @@ class Page4_Double_Sim_Window(QFrame):
         #                    self.lineedit_radius, self.lineedit_max_speed, self.lineedit_width, self.lineedit_overlap]
 
         # self.initLineEditsWidth()
+
     # def on_button_clicked(self):
     #     for line_edit in self.line_edits:
     #         if not line_edit.text().strip():  # 如果任何一个LineEdit为空
@@ -289,45 +298,101 @@ class Page4_Double_Sim_Window(QFrame):
         self.button_sync_swing_simulation.setText('同步摆抛磨量分布仿真')
         self.button_sync_swing_middle_line.setText('同步摆轨迹中心线绘制')
         self.button_sync_swing_animation.setText('同步摆动画')
+        self.button_save.setText('保存参数')
+        self.button_save.clicked.connect(self.saveSettings)
 
         # self.button_save.setText('保存参数')
         # self.button_save.clicked.connect(self.saveSettings)
 
     def loadSettings(self):
         """加载配置文件中的数据到各个LineEdit控件"""
-        # self.lineedit_gap.setText(self.settings.value("lineedit_gap1", ""))
-        # self.lineedit_size.setText(self.settings.value("lineedit_size1", ""))
-        # self.lineedit_belt_speed.setText(self.settings.value("lineedit_belt_speed1", ""))
-        # self.lineedit_acc.setText(self.settings.value("lineedit_acc1", ""))
-        # self.lineedit_radius.setTex(self.settings.value("lineedit_radius1", ""))
-        # self.lineedit_width.setText(self.settings.value("lineedit_width1", ""))
-        # self.lineedit_max_speed.setText(self.settings.value("lineedit_max_speed1", ""))
-        # self.lineedit_overlap.setText(self.settings.value("lineedit_overlap1", ""))
-        # self.lineedit_beam_speed.setText(self.settings.value("lineedit_beam_speed", ""))
-        # self.lineedit_swing.setText(self.settings.value("lineedit_swing", ""))
-        # self.lineedit_stop_time.setText(self.settings.value("lineedit_stop_time", ""))
-        # self.lineedit_swing_time.setText(self.settings.value("lineedit_swing_time", ""))
-        # self.lineedit_coefficient.setText(self.settings.value("lineedit_coefficient", ""))
-        # self.lineedit_amount.setText(self.settings.value("lineedit_amount", ""))
+        self.lineedit_mod_between.setText(self.settings.value("lineedit_mod_between4", ""))
+        self.lineedit_beam_swing_time.setText(self.settings.value("lineedit_beam_swing_time4", ""))
+        self.lineedit_belt_speed.setText(self.settings.value("lineedit_belt_speed4", ""))
+        self.lineedit_between_beam.setText(self.settings.value("lineedit_between_beam4", ""))
+        self.lineedit_stay_time.setText(self.settings.value("lineedit_stay_time4", ""))
+        self.lineedit_beam_swing_speed.setText(self.settings.value("lineedit_beam_swing_speed4", ""))
+        self.lineedit_mod_count.setText(self.settings.value("lineedit_mod_count4", ""))
+        self.lineedit_mode_size.setText(self.settings.value("lineedit_mode_size4", ""))
+        self.lineedit_accelerate_speed.setText(self.settings.value("lineedit_accelerate_speed4", ""))
+        self.lineedit_radius.setText(self.settings.value("lineedit_radius4", ""))
+        self.lineedit_delay_time.setText(self.settings.value("lineedit_delay_time4", ""))
 
     def saveSettings(self):
         """保存各个LineEdit控件的数据到配置文件"""
-        # self.settings.setValue("lineedit_gap1", self.lineedit_gap.text())
-        # self.settings.setValue("lineedit_size1", self.lineedit_size.text())
-        # self.settings.setValue("lineedit_belt_speed1", self.lineedit_belt_speed.text())
-        # self.settings.setValue("lineedit_acc1", self.lineedit_acc.text())
-        # self.settings.setValue("lineedit_radius1", self.lineedit_radius.text())
-        # self.settings.setValue("lineedit_width1", self.lineedit_width.text())
-        # self.settings.setValue("lineedit_max_speed1", self.lineedit_max_speed.text())
-        # self.settings.setValue("lineedit_overlap1", self.lineedit_overlap.text())
-        # self.settings.setValue("lineedit_beam_speed", self.lineedit_beam_speed.text())
-        # self.settings.setValue("lineedit_swing", self.lineedit_swing.text())
-        # self.settings.setValue("lineedit_stop_time", self.lineedit_stop_time.text())
-        # self.settings.setValue("lineedit_swing_time", self.lineedit_swing_time.text())
-        # self.settings.setValue("lineedit_coefficient", self.lineedit_coefficient.text())
-        # self.settings.setValue("lineedit_amount", self.lineedit_amount.text())
+        self.settings.setValue("lineedit_mod_between4", self.lineedit_mod_between.text())
+        self.settings.setValue("lineedit_beam_swing_time4", self.lineedit_beam_swing_time.text())
+        self.settings.setValue("lineedit_belt_speed4", self.lineedit_belt_speed.text())
+        self.settings.setValue("lineedit_between_beam4", self.lineedit_between_beam.text())
+        self.settings.setValue("lineedit_stay_time4", self.lineedit_stay_time.text())
+        self.settings.setValue("lineedit_beam_swing_speed4", self.lineedit_beam_swing_speed.text())
+        self.settings.setValue("lineedit_mod_count4", self.lineedit_mod_count.text())
+        self.settings.setValue("lineedit_mode_size4", self.lineedit_mode_size.text())
+        self.settings.setValue("lineedit_accelerate_speed4", self.lineedit_accelerate_speed.text())
+        self.settings.setValue("lineedit_radius4", self.lineedit_radius.text())
+        self.settings.setValue("lineedit_delay_time4", self.lineedit_delay_time.text())
 
     def closeEvent(self, event):
         """在窗口关闭时调用保存设置函数"""
         self.saveSettings()
         super().closeEvent(event)
+
+    def start_computation_trajectory_animation_equal(self):
+        self.trajectory_animation_thread = Animation_produce_equal(float(self.lineedit_belt_speed.text()),
+                                                                   float(self.lineedit_beam_swing_speed.text()),
+                                                                   float(self.lineedit_beam_swing_time.text()),
+                                                                   float(self.lineedit_stay_time.text()),
+                                                                   float(self.lineedit_accelerate_speed.text()),
+                                                                   float(self.lineedit_radius.text()),
+                                                                   float(self.lineedit_mod_between.text()),
+                                                                   float(self.lineedit_between_beam.text()),
+                                                                   math.ceil(float(self.lineedit_mod_count.text())),
+                                                                   )
+        self.trajectory_animation_thread.result_ready.connect(self.trajectory_animation_ready)
+        self.button_sync_swing_animation.setEnabled(False)
+        # 运行子线程
+        self.trajectory_animation_thread.start()
+
+    def start_computation_trajectory_animation_order(self):
+        self.trajectory_animation_thread = Animation_produce_order(float(self.lineedit_belt_speed.text()),
+                                                                   float(self.lineedit_beam_swing_speed.text()),
+                                                                   float(self.lineedit_beam_swing_time.text()),
+                                                                   float(self.lineedit_stay_time.text()),
+                                                                   float(self.lineedit_accelerate_speed.text()),
+                                                                   float(self.lineedit_radius.text()),
+                                                                   float(self.lineedit_mod_between.text()),
+                                                                   float(self.lineedit_between_beam.text()),
+                                                                   math.ceil(float(self.lineedit_mod_count.text())),
+                                                                   float(self.lineedit_delay_time.text())
+                                                                   )
+        self.trajectory_animation_thread.result_ready.connect(self.trajectory_animation_ready)
+        self.button_order_swing_animation.setEnabled(False)
+        # 运行子线程
+        self.trajectory_animation_thread.start()
+
+    def start_computation_trajectory_animation_cross(self):
+        self.trajectory_animation_thread = Animation_produce_cross(float(self.lineedit_belt_speed.text()),
+                                                                   float(self.lineedit_beam_swing_speed.text()),
+                                                                   float(self.lineedit_beam_swing_time.text()),
+                                                                   float(self.lineedit_stay_time.text()),
+                                                                   float(self.lineedit_accelerate_speed.text()),
+                                                                   float(self.lineedit_radius.text()),
+                                                                   float(self.lineedit_mod_between.text()),
+                                                                   float(self.lineedit_between_beam.text()),
+                                                                   math.ceil(float(self.lineedit_mod_count.text())),
+                                                                   )
+        self.trajectory_animation_thread.result_ready.connect(self.trajectory_animation_ready)
+        self.button_cross_swing_animation.setEnabled(False)
+        # 运行子线程
+        self.trajectory_animation_thread.start()
+
+    def trajectory_animation_ready(self, str_22):
+        # 加载GIF动画
+        print(str_22)
+        self.movie = QMovie("animation.gif")
+        # self.movie.setloopCount(1)  # 设置只播放一次
+        self.label_gif.setMovie(self.movie)
+        self.movie.start()
+        self.button_order_swing_animation.setEnabled(True)
+        self.button_sync_swing_animation.setEnabled(True)
+        self.button_cross_swing_animation.setEnabled(True)
